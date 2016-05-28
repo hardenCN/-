f8fb12134c8754279014e22b085367e8c1bb2ffe
Harden Zhang <zdp118@gmail.com>
2016-05-28 11:47:30 +0800

    code
    code

---
+++ util.js
@@ -0,0 +1,69 @@
+/**
+ * Created by pans on 15/12/6.
+ */
+'use strict';
+
+
+var fs = require('fs')
+    , gm = require('gm').subClass({imageMagick: true});
+var qr = require('qr-image');
+
+// var qr_svg = qr.image('https://dearii.com', { type: 'png' });
+// qr_svg.pipe(fs.createWriteStream('i_love_qr.png'));
+// creating an image
+// gm('miyu_main.png')
+//     .composite(qr_svg)
+//     .geometry('+200+200')
+//     // .stream(function (err, stdout, stderr) {
+//     //     var writeStream = fs.createWriteStream('resized.jpg');
+//     //     stdout.pipe(writeStream);
+//     // });
+//     .write("brandNewImg.jpg", function (err) {
+//         if (err) throw err;
+//         else console.log('done');
+//     });
+var path = "D:\\Documents\\文件\\网站上用的照片\\1000X667凤凰岛游艇，标志海景酒店4个\\1000x667";
+
+function explorer(path) {
+    fs.readdir(path, function (err, files) {
+        if (err) {
+            console.log("error:\n" + err);
+            return;
+        }
+        files.forEach(function (file) {
+            fs.stat(path + "\\" + file + '',
+                function (err, stat) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    // if (stat.isDirectory()) {
+                    //     console.log(path + "\\" + file + "\\");
+                    //     explorer(path + "\\" + file);
+                    // } else {
+                        console.log(path + "\\" + file);
+
+                        gm(path + "\\" + file)
+                            .composite('2.png')
+                            .geometry('+130+550')
+                            .write("shuiyin/" + file, function (err) {
+                                if (err) throw err;
+                                else console.log('done');
+                            });
+                    // }
+
+                }
+            )
+            ;
+        });
+    })
+}
+explorer(path);
+// gm('1.jpg')
+//     .composite('2.png')
+//     .geometry('+40+290')
+//     .write('composite.png', function (err) {
+//         if (err) throw err;
+//         else console.log('done');
+//     });
+
